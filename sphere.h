cf016e9cbed610e25e64ddc99b016dc893af0275
steeeeeeeeel <steel.cooper@proton.me>
2026-08-30 15:53:27 +1000

    Add movement suport to sphere

--- sphere.h
+++ sphere.h
@@ -6,11 +6,17 @@
 
 class sphere : public hittable {
     public:
-        sphere(const point3& centre, double radius, shared_ptr<material> mat)
-            : centre(centre), radius(fmax(0,radius)), mat(mat) {}
+        // Static
+        sphere(const point3& static_centre, double radius, shared_ptr<material> mat)
+            : centre(static_centre, vec3(0,0,0)), radius(fmax(0,radius)), mat(mat) {}
+
+        // Animated
+        sphere(const point3& centre1, const point3& centre2, double radius, shared_ptr<material> mat)
+            : centre(centre1, centre2 - centre1), radius(fmax(0,radius)), mat(mat) {}
 
         bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
-            vec3 oc = centre - r.origin();
+            point3 current_centre = centre.at(r.time());
+            vec3 oc = current_centre - r.origin();
             auto a = r.direction().length_squared();
             auto h = dot(r.direction(), oc);
             auto c = oc.length_squared() - radius*radius;
@@ -29,7 +35,7 @@ class sphere : public hittable {
 
             rec.t = root;
             rec.p = r.at(rec.t);
-            vec3 outward_normal = (rec.p - centre) / radius;
+            vec3 outward_normal = (rec.p - current_centre) / radius;
             rec.set_face_normal(r, outward_normal);
             rec.mat = mat;
 
@@ -37,7 +43,7 @@ class sphere : public hittable {
         }
 
     private:
-        point3 centre;
+        ray centre;
         double radius;
         shared_ptr<material> mat;
 };
